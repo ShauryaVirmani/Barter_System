45ccbc415eb4880af04ff8b7caf8dab7e97e2600
ShauryaVirmani <63544164+ShauryaVirmani@users.noreply.github.com>
2021-04-25 13:47:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen'
 
 export default function App() {
-  return  <SignupLoginScreen/>
+  return  <WelcomeScreen/>
     
   
 }
