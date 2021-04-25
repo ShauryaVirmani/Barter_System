227f08813970de5e66ee42a934854b8d87b57c41
ShauryaVirmani <63544164+ShauryaVirmani@users.noreply.github.com>
2021-04-25 12:51:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignupLoginScreen from './screens/WelcomeScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
 
 export default function App() {
   return  <SignupLoginScreen/>
